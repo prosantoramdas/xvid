610e940892929443723667c597e255ea51896657
prosantoramdas <136964226+prosantoramdas@users.noreply.github.com>
2024-01-17 18:55:05 +0600

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const puppeteer = require('puppeteer');
 
 const app = express();
 const port = process.env.PORT || 3000;
+app.get('/', (req,res) =>{
+  res.send('hi')
+})
 
 app.get('/scrape', async (req, res) => {
   try {
